4594e1d7dcf91421b3caeaf0ac5c186cf86d8e36
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-02 12:08:33 -0700

    Linklardagi xatoliklarini tahrirlash mustaqil

--- TopTransfers/urls.py
+++ TopTransfers/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('clubs/', clubs),
+    path('about/', about),
     path('players/', players),
     path('stats/', stats),
     path('transfers_records/', transfers_records),
