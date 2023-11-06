3ea81f41c2c8d2c97c0e30fd2f898293d953a41d
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-06 01:12:59 -0800

    Admin panelda playerda qidirish, clubi bo'yicha filterlash, yangi player qo'shishda clubini ham qidirish imkoniyati va tahrirlar

--- asosiy/admin.py
+++ asosiy/admin.py
@@ -4,14 +4,25 @@ from .models import *
 
 @admin.register(Club)
 class ClubAdmin(admin.ModelAdmin):
-    list_display = ["nom", "davlat", "president", "coach", "yili"]
+    list_display = ["id", "nom", "davlat", "president", "coach", "yili"]
     search_fields = ["nom", "davlat__nom"]
-    search_help_text = "Nom ustunlari bo'yicha qidiring"
+    search_help_text = "Nom, Davlat ustunlari bo'yicha qidiring"
     list_filter = ["davlat__nom"]
+    ordering = ["id"]
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ["id", "ism", "davlat", "tugulgan_sana", "narx", "club", "pozitsiya"]
+    search_fields = ["ism", "davlat__nom", "club__nom"]
+    search_help_text = "Ism, Davlat va Club ustunlari bo'yicha qidiring"
+    list_filter = ["club"]
+    autocomplete_fields = ["club"]
+    ordering = ["id"]
 
 
 admin.site.register(Davlat)
 # admin.site.register(Club)
-admin.site.register(Player)
+# admin.site.register(Player)
 admin.site.register(Transfer)
 admin.site.register(HMavsum)
