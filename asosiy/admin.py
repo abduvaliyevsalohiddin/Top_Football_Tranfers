2b736b1ef84630a2591a65d88a9c6058d3ca6cc7
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-06 00:58:55 -0800

    Admin panelda club jadvaliga qidirish, davlati bo'yicha filterlash imkoniyati

--- asosiy/admin.py
+++ asosiy/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import *
 
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ["nom", "davlat", "president", "coach", "yili"]
+    search_fields = ["nom", "davlat__nom"]
+    search_help_text = "Nom ustunlari bo'yicha qidiring"
+    list_filter = ["davlat__nom"]
+
+
 admin.site.register(Davlat)
-admin.site.register(Club)
+# admin.site.register(Club)
 admin.site.register(Player)
 admin.site.register(Transfer)
 admin.site.register(HMavsum)
